5f5221457aa4e5f465f62c9b6241243b1bdf34c5
machukhinktato <tarabrinmv@gmail.com>
2020-05-05 12:45:19 +0300

    possibility to edit tag  added

--- blog/views.py
+++ blog/views.py
@@ -16,28 +16,6 @@ class PostCreate(ObjectCreateMixin, View):
     model_form = PostForm
     template = 'blog/post_create_form.html'
 
-    # def get(self, request):
-    #     form = PostForm()
-    #
-    #     return render(
-    #         request,
-    #         'blog/post_create_form.html',
-    #         context={'form': form}
-    #     )
-    #
-    # def post(self, request):
-    #     bound_form = PostForm(request.POST)
-    #
-    #     if bound_form.is_valid():
-    #         new_post =  bound_form.save()
-    #         return redirect(new_post)
-    #
-    #     return render(
-    #         request,
-    #         'blog/post_create_form.html',
-    #         context={'form': bound_form}
-    #     )
-
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
@@ -48,30 +26,31 @@ class TagCreate(ObjectCreateMixin, View):
     model_form = TagForm
     template = 'blog/tag_create.html'
 
-    # def get(self, request):
-    #     form = TagForm()
-    #     return render(request, 'blog/tag_create.html', context={'form': form})
-    #
-    # def post(self, request):
-    #     bound_form = TagForm(request.POST)
-    #     if bound_form.is_valid():
-    #         new_tag = bound_form.save()
-    #         return redirect(new_tag)
-    #     return render(request, 'blog/tag_create.html', context={'form': bound_form})
 
+class TagUpdate(View):
+    # noinspection PyMethodMayBeStatic
+    def get(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        bound_form = TagForm(instance=tag)
 
-# class PostDetail(View):
-#     def get(self, request, slug):
-#         post = Post.objects.get(slug__iexact=slug)
-# post = get_object_or_404(Post, slug__iexact=slug)
-# return render(request, 'blog/post_detail.html', context={'post': post})
+        return render(
+            request,
+            'blog/tag_update_form.html',
+            context={'form': bound_form, 'tag': tag})
 
+    # noinspection PyMethodMayBeStatic
+    def post(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        bound_form = TagForm(request.POST, instance=tag)
 
-# class TagDetail(View):
-#     def get(self, request, slug):
-#         tag = Tag.objects.get(slug__iexact=slug)
-# tag = get_object_or_404(Tag, slug__iexact=slug)
-# return render(request, 'blog/tag_detail.html', context={'tag': tag})
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+
+        return render(
+            request,
+            'blog/tag_update_form.html',
+            context={'form': bound_form, 'tag': tag})
 
 
 def posts_list(request):
