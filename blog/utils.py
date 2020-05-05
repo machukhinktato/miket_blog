f4c4537faaee29c3a2415f77b3b69999e3cff785
machukhinktato <tarabrinmv@gmail.com>
2020-05-05 11:26:35 +0300

    user could create tag and posts, post method activated

--- blog/utils.py
+++ blog/utils.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 
 from .models import *
+
 
 class ObjectDetailMixin:
     model = None
@@ -12,5 +14,28 @@ class ObjectDetailMixin:
         return render(
             request,
             self.template,
-            context={self.model.__name__.lower():obj}
+            context={self.model.__name__.lower(): obj}
+        )
+
+
+class ObjectCreateMixin:
+    model_form = None
+    template = None
+
+    def get(self, request):
+        form = self.model_form()
+        return render(request, self.template, context={'form': form})
+
+    def post(self, request):
+        bound_form = self.model_form(request.POST)
+
+        if bound_form.is_valid():
+            new_obj = bound_form.save()
+            return redirect(new_obj)
+        return render(
+            request,
+            self.template,
+            context={
+                'form': bound_form
+            }
         )
