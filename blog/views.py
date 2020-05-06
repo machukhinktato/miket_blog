cb34d5c343506fef7e60b662c157eed8c68e8da9
machukhinktato <tarabrinmv@gmail.com>
2020-05-06 17:00:39 +0300

    buttons next and previous attached

--- blog/views.py
+++ blog/views.py
@@ -65,11 +65,29 @@ def posts_list(request):
     paginator = Paginator(posts, 2)
     page_number = request.GET.get('page', 1)
     page = paginator.get_page(page_number)
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = f'?page={page.previous_page_number()}'
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = f'?page={page.next_page_number()}'
+    else:
+        next_url = ''
+
+    context = {
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'next_url': next_url,
+        'prev_url': prev_url,
+    }
 
     return render(
         request,
         'blog/index.html',
-        context={'page_object': page}
+        context=context
     )
 
 
